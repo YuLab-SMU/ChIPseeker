97c2567ea10f04f9862e5fff3bde77a4007bbe1c
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-31 16:21:19 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commits.
    Commit information:
    
    Commit id: 41831a70cbf3c8f0c04d1438722b6ecff5227aaa
    Commit message: Merge https://github.com/GuangchuangYu/ChIPseeker
    move to github
    Committed by: Guangchuang Yu
    Author Name: Guangchuang Yu
    Commit date: 2014-06-01 00:20:06 +0800
    Author date: 2014-06-01 00:20:06 +0800
    
    Commit id: 071cc3e4e07060eaf99f561b90c8840a5e07e3bd
    Commit message: move to github
    Committed by: Guangchuang Yu
    Author Name: Guangchuang Yu
    Commit date: 2014-06-01 00:17:28 +0800
    Author date: 2014-06-01 00:17:28 +0800
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChIPseeker@90898 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/enrichOverlap.R
@@ -0,0 +1,134 @@
+##' calcuate overlap significant of ChIP experiments based on their nearest gene annotation
+##'
+##' 
+##' @title enrichAnnoOverlap
+##' @param queryPeak query bed file
+##' @param targetPeak target bed file(s) or folder containing bed files
+##' @param TranscriptDb TranscriptDb
+##' @param pAdjustMethod pvalue adjustment method
+##' @return data.frame
+##' @export
+##' @author G Yu
+enrichAnnoOverlap <- function(queryPeak, targetPeak, TranscriptDb=NULL, pAdjustMethod="BH") {
+    targetFiles <- parse_targetPeak_Param(targetPeak)
+    TranscriptDb <- loadTxDb(TranscriptDb)
+ 
+    query.anno <- annotatePeak(queryPeak, TranscriptDb=TranscriptDb,
+                               assignGenomicAnnotation=FALSE, annoDb=NULL, verbose=FALSE)
+    
+    target.anno <- lapply(targetFiles, annotatePeak, TranscriptDb=TranscriptDb,
+                          assignGenomicAnnotation=FALSE, annoDb=NULL, verbose=FALSE)
+    
+    ChIPseekerEnv <- get("ChIPseekerEnv", envir=.GlobalEnv)
+    features <- get("features", envir=ChIPseekerEnv)
+    ol <- lapply(target.anno, function(i) intersect(query.anno$geneId, i$geneId))
+    oln <- unlist(lapply(ol, length))
+    N <- length(features)
+    ## white ball
+    m <- length(unique(query.anno$geneId))
+    ## black ball
+    n <- N - m
+    ## drawn
+    k <- unlist(lapply(target.anno, function(i) length(unique(i$geneId))))
+    p <- phyper(oln, m, n, k, lower.tail=FALSE)
+    qSample <- sub(".+/", "", queryPeak)
+    tSample <- sub(".+/", "", targetFiles)
+    padj <- p.adjust(p, method=pAdjustMethod)
+    res <- data.frame(qSample=qSample,
+                      tSample=tSample,
+                      qLen=length(query.anno$geneId),
+                      tLen=unlist(lapply(target.anno, length)),
+                      N_OL=oln,
+                      pvalue=p,
+                      p.adjust=padj)
+    return(res)
+}
+
+##' calculate overlap significant of ChIP experiments based on the genome coordinations
+##'
+##' 
+##' @title enrichPeakOverlap
+##' @param queryPeak query bed file
+##' @param targetPeak target bed file(s) or folder that containing bed files
+##' @param TranscriptDb TranscriptDb
+##' @param pAdjustMethod pvalue adjustment method
+##' @param nShuffle shuffle numbers
+##' @return data.frame
+##' @export
+##' @author G Yu
+enrichPeakOverlap <- function(queryPeak, targetPeak, TranscriptDb=NULL, pAdjustMethod="BH", nShuffle=1000) {
+    targetFiles <- parse_targetPeak_Param(targetPeak)
+    TranscriptDb <- loadTxDb(TranscriptDb)
+    query.gr <- loadPeak(queryPeak)
+    target.gr <- lapply(targetFiles, loadPeak)
+    p.ol <- enrichOverlap.peak.internal(query.gr, target.gr, TranscriptDb, nShuffle)
+    p <- p.ol$pvalue
+    ol <- p.ol$overlap
+    qSample <- sub(".+/", "", queryPeak)  ## remove path, only keep file name
+    tSample <- sub(".+/", "", targetFiles) 
+    padj <- p.adjust(p, method=pAdjustMethod)
+    res <- data.frame(qSample=qSample,
+                      tSample=tSample,
+                      qLen=length(query.gr),
+                      tLen=unlist(lapply(target.gr, length)),
+                      N_OL=ol,
+                      pvalue=p,
+                      p.adjust=padj)
+    
+    return(res)
+}
+
+
+
+##' shuffle the position of peak
+##'
+##' 
+##' @title shuffle
+##' @param peak.gr GRanges object
+##' @param TranscriptDb TranscriptDb
+##' @return GRanges object
+##' @export
+##' @author G Yu
+shuffle <- function(peak.gr, TranscriptDb) {
+    chrLens <- seqlengths(TranscriptDb)[names(seqlengths(peak.gr))]
+    nn <- as.vector(seqnames(peak.gr))
+    ii <- order(nn)
+    w <- width(peak.gr)
+    nnt <- table(nn)
+    jj <- order(names(nnt))
+    nnt <- nnt[jj]
+    chrLens <- chrLens[jj]
+    ss <- unlist(sapply(1:length(nnt), function(i) sample(chrLens[i],nnt[i])))
+
+    res <- GRanges(seqnames=nn[ii], ranges=IRanges(ss, ss+w[ii]), strand="*")
+    return(res)   
+}
+
+
+
+
+##' @importFrom GenomeInfoDb intersect
+##' @importFrom GenomeInfoDb seqlengths
+enrichOverlap.peak.internal <- function(query.gr, target.gr, TranscriptDb, nShuffle=1000) {
+    idx <- sample(1:length(target.gr), nShuffle, replace=TRUE)
+
+    len <- unlist(lapply(target.gr, length))
+    
+    rr <- mclapply(idx, function(i) {
+        tarShuffle <- shuffle(target.gr[[i]], TranscriptDb)
+        length(intersect(query.gr, tarShuffle))/len[i]
+    }, mc.cores=detectCores()-1
+                   )
+    rr <- unlist(rr) ## random ratio
+
+    qLen <- mclapply(target.gr, function(tt) {
+        length(intersect(query.gr, tt))
+    }, mc.cores=detectCores()-1
+                  )
+    ## query ratio
+    qr <- qLen/len
+    p <- lapply(qr, function(q) mean(rr>q))
+    res <- list(pvalue=unlist(p), overlap=qLen)
+    return(res)
+}
+
---
+++ R/plotDistToTSS.R
@@ -0,0 +1,129 @@
+##' plot feature distribution based on the distances to the TSS
+##'
+##' 
+##' @title plotDistToTSS
+##' @param peakAnno peak annotation
+##' @param distanceColumn column name of the distance from peak to nearest gene
+##' @param xlab x label
+##' @param ylab y lable
+##' @param title figure title
+##' @return bar plot that summarize distance from peak to
+##' TSS of the nearest gene.
+##' @importFrom plyr ddply
+##' @importFrom plyr .
+##' @importFrom plyr summarise
+##' @importFrom ggplot2 ggplot
+##' @importFrom ggplot2 aes
+##' @importFrom ggplot2 aes_string
+##' @importFrom ggplot2 geom_bar
+##' @importFrom ggplot2 geom_hline
+##' @importFrom ggplot2 theme_bw
+##' @importFrom ggplot2 coord_flip
+##' @importFrom ggplot2 xlab
+##' @importFrom ggplot2 ylab
+##' @importFrom ggplot2 ggtitle
+##' @importFrom ggplot2 geom_hline
+##' @importFrom ggplot2 scale_y_continuous
+##' @importFrom ggplot2 scale_x_continuous
+##' @importFrom ggplot2 scale_fill_brewer
+##' @importFrom ggplot2 scale_fill_hue
+##' @importFrom ggplot2 scale_fill_manual
+##' @importFrom ggplot2 geom_text
+##' @examples
+##' require(TxDb.Hsapiens.UCSC.hg19.knownGene)
+##' txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+##' peakfile <- system.file("extdata", "sample_peaks.txt", package="ChIPseeker")
+##' peakAnno <- annotatePeak(peakfile, TranscriptDb=txdb)
+##' plotDistToTSS(peakAnno)
+##' @seealso \code{\link{annotatePeak}}
+##' @export
+##' @author G Yu
+plotDistToTSS <- function(peakAnno,
+                          distanceColumn="distanceToTSS", 
+                          xlab="", ylab="Binding sites (%) (5'->3')",
+                          title="Distribution of transcription factor-binding loci relative to TSS") {
+
+    ## to satisfy codetools
+    Feature <- freq <- NULL
+
+    if (class(peakAnno) == "GRanges")
+        peakAnno <- as.data.frame(peakAnno)
+    
+    if (class(peakAnno) == "data.frame") {
+        peakDist <- peakAnno
+        categoryColumn <- 1
+    } else if (class(peakAnno) == "list") {
+        peakAnno <- lapply(peakAnno, as.data.frame)
+        peakDist <- ldply(peakAnno)
+        categoryColumn <- ".id"
+    } else {
+        stop("peakAnno should be a data.frame or a named list of data.frame.")
+    }
+    
+    ## assign Feature according to the distancetoFeature
+    peakDist$Feature <- NA
+    limit <- c(0, 1000, 3000, 5000, 10000, 100000)
+    lbs <- c("0-1kb", "1-3kb", "3-5kb", "5-10kb", "10-100kb", ">100kb")
+    for (i in 1:length(limit)) {
+	if (i < length(limit)) {
+            peakDist$Feature[ abs(peakDist[, distanceColumn]) >= limit[i] & abs(peakDist[,distanceColumn]) < limit[i+1] ] <- lbs[i]
+	} else {
+            peakDist$Feature[abs(peakDist[,distanceColumn]) > limit[i]] <- lbs[i]
+	}
+    }
+    peakDist$Feature <- factor(peakDist$Feature, levels=lbs)
+
+    ## sign containing -1 and 1 for upstream and downstream
+    peakDist$sign <- sign(peakDist[,distanceColumn])
+
+    ## count frequencies
+    if (categoryColumn == 1) {
+        peakDist <- ddply(peakDist, .(Feature, sign), summarise, freq=length(Feature))
+        peakDist$freq = peakDist$freq/sum(peakDist$freq)
+        peakDist$freq = peakDist$freq * 100
+        totalFreq <- ddply(peakDist, .(sign), summarise, total=sum(freq))
+    } else {
+        peakDist <- ddply(peakDist, c(categoryColumn, "Feature", "sign"), summarise, freq=length(Feature))
+        nn <- unique(peakDist[, categoryColumn])
+        for (i in 1:length(nn)) {
+            idx <- peakDist[, categoryColumn] == nn[i]
+            peakDist$freq[idx] <- peakDist$freq[idx]/sum(peakDist$freq[idx])
+        }
+        peakDist$freq = peakDist$freq * 100
+        totalFreq <- ddply(peakDist, c(categoryColumn, "sign"), summarise, total=sum(freq))
+    }
+
+    ## preparing ylim and y tick labels
+    ds = max(totalFreq$total[totalFreq$sign == 1])
+    dslim = ceiling(ds/10) * 10
+    us = max(totalFreq$total[totalFreq$sign == -1])
+    uslim = ceiling(us/10) * 10
+    ybreaks <- seq(-uslim, dslim, by=10)
+    ylbs <- abs(ybreaks)
+    ylbs[ylbs == 0] <- "TSS"
+    
+    if (categoryColumn == 1) {
+        p <- ggplot(peakDist, aes(x=1, fill=Feature))
+    } else {
+        p <- ggplot(peakDist, aes_string(x=categoryColumn, fill="Feature"))
+    }
+    
+    p <- p + geom_bar(subset=.(sign==1), aes(y=freq), stat="identity") + 
+        geom_bar(subset=.(sign==-1), aes(y=-freq), stat="identity")
+    
+    p <- p + geom_hline(yintercept = 0, colour = "black") +
+        coord_flip() + theme_bw() +
+            scale_y_continuous(breaks=ybreaks,labels=ylbs)
+    
+    p <- p + ylab(ylab) + xlab(xlab) + ggtitle(title) 
+
+    if (categoryColumn == 1) {
+        p <- p + scale_x_continuous(breaks=NULL)
+    }
+    ## p <- p + scale_fill_hue("Feature", breaks=lbs, labels=lbs)
+    p <- p + scale_fill_manual(values=getCols(length(lbs)), breaks=lbs, labels=lbs)
+    
+    return(p)
+}
+
+
