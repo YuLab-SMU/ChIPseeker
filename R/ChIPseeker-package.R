6db2a37a293b77d388ddf6bf5ed0fc974514ccbd
Guangchuang Yu <guangchuangyu@gmail.com>
2014-08-14 11:21:01 +0800

    bug fixed in plotDistToTSS

--- R/ChIPseeker-package.R
+++ R/ChIPseeker-package.R
@@ -4,7 +4,7 @@
 #' This package is designed for chip-seq data analysis
 #'
 #' \tabular{ll}{ Package: \tab ChIPseeker\cr Type: \tab Package\cr Version: \tab
-#' 0.99.0\cr Date: \tab 2-01-2014\cr biocViews:\tab ChIPSeq,
+#' 1.1.14\cr Date: \tab 2-01-2014\cr biocViews:\tab ChIPSeq,
 #' Annotation, Software\cr Depends:\tab \cr Imports: \tab methods, ggplot2
 #' \cr Suggests:\tab clusterProfiler, GOSemSim\cr License: \tab
 #' Artistic-2.0\cr }
