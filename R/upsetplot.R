073669b47edc75bdc80dd74e44202ac3b144e9de
GuangchuangYu <guangchuangyu@gmail.com>
2015-07-20 17:11:52 +0800

    add vennpie parameter in upsetplot

--- R/upsetplot.R
+++ R/upsetplot.R
@@ -1,6 +1,11 @@
 ##' @importFrom UpSetR upset
+##' @importFrom grid viewport
+##' @importFrom grid pushViewport
+##' @importFrom grid popViewport
+##' @importFrom gridBase gridPLT
+##' @importFrom graphics plot.new
 ##' @author Guangchuang Yu
-upsetplot.csAnno <- function(x, sets=NULL, order.matrix = "freq", ...) {
+upsetplot.csAnno <- function(x, sets=NULL, order.matrix = "freq", sets.bar.color=NULL, vennpie=FALSE, ...) {
     y <- x@detailGenomicAnnotation
     y <- as.matrix(y)
     y[y] <- 1
@@ -14,6 +19,27 @@ upsetplot.csAnno <- function(x, sets=NULL, order.matrix = "freq", ...) {
         sets <- c("distal_intergenic", "downstream",
                   "threeUTR", "fiveUTR", "Intron",
                   "Exon", "Promoter")
+        if (vennpie && is.null(sets.bar.color)) {
+            sets.bar.color <- c("#d95f0e", "#fee0d2", "#98D277",
+                                "#6F9E4C", "#fc9272", "#9ecae1", "#ffeda0")
+        } 
     }
-    upset(y, sets=sets, order.matrix = order.matrix, ...)
+    
+    if (is.null(sets.bar.color)) {
+        sets.bar.color <- "black"
+    }
+
+    if (vennpie) {
+        plot.new()
+        # grid.rect(gp = gpar(fill="white"))
+        upset(y, sets=sets, sets.bar.color=sets.bar.color,
+              order.matrix = order.matrix, ...)
+        pushViewport(viewport(x=.6, y=.7, width=.6, height=.6))
+        par(plt=gridPLT(), new=TRUE)
+        vennpie(x)
+        popViewport()
+    } else {
+        upset(y, sets=sets,sets.bar.color=sets.bar.color,
+              order.matrix = order.matrix, ...)
+    }
 }
