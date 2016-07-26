eb238ee6d82cae4ab13ebce697727b3a7b821ead
GuangchuangYu <guangchuangyu@gmail.com>
2016-07-26 11:45:24 +0800

    :bug: fixed

--- R/enrichOverlap.R
+++ R/enrichOverlap.R
@@ -16,13 +16,19 @@
 ##' @importFrom rtracklayer liftOver
 ##' @author G Yu
 enrichAnnoOverlap <- function(queryPeak, targetPeak, TxDb=NULL, pAdjustMethod="BH", chainFile=NULL, distanceToTSS_cutoff=NULL) {
-    targetFiles <- parse_targetPeak_Param(targetPeak)
     TxDb <- loadTxDb(TxDb)
- 
+
     query.anno <- annotatePeak(queryPeak, TxDb=TxDb,
                                assignGenomicAnnotation=FALSE, annoDb=NULL, verbose=FALSE)
 
-    target.gr <- lapply(targetFiles, loadPeak)
+    
+    if (is(targetPeak[1], "GRanges") || is(targetPeak[[1]], "GRanges")) {
+        target.gr <- targetPeak
+    } else {
+        targetFiles <- parse_targetPeak_Param(targetPeak)
+        target.gr <- lapply(targetFiles, loadPeak)
+    }
+
     if (!is.null(chainFile)) {
         chain <- import.chain(chainFile)
         target.gr <- lapply(target.gr, liftOver, chain=chain)
@@ -93,6 +99,7 @@ enrichPeakOverlap <- function(queryPeak, targetPeak, TxDb=NULL, pAdjustMethod="B
     query.gr <- loadPeak(queryPeak)
     if (is(targetPeak[1], "GRanges") || is(targetPeak[[1]], "GRanges")) {
         target.gr <- targetPeak
+        targetFiles <- NULL
     } else {
         targetFiles <- parse_targetPeak_Param(targetPeak)
         target.gr <- lapply(targetFiles, loadPeak)
@@ -129,8 +136,21 @@ enrichPeakOverlap <- function(queryPeak, targetPeak, TxDb=NULL, pAdjustMethod="B
     }
         
     ol <- p.ol$overlap
-    qSample <- sub(".+/", "", queryPeak)  ## remove path, only keep file name
-    tSample <- sub(".+/", "", targetFiles) 
+    if (is(queryPeak, "GRanges")) {
+        qSample <- "queryPeak"
+    } else {
+        ## remove path, only keep file name
+        qSample <- basename(queryPeak)  
+    }
+
+    if (is.null(targetFiles)) {
+        tSample <- names(target.gr)
+        if(is.null(tSample)) {
+            tSample <- paste0("targetPeak", seq_along(target.gr))
+        }
+    } else {
+        tSample <- basename(".+/", "", targetFiles)
+    }
     
     res <- data.frame(qSample=qSample,
                       tSample=tSample,
