2393947595edace1bfa07c3e2d4471dacf05690b
xuzhougeng <18868192325m0@sina.cn>
2019-08-23 16:02:35 +0800

    add subset function for csAnno

---
+++ R/subset.R
@@ -0,0 +1,26 @@
+##' @importFrom S4Vectors subset
+##' @importFrom S4Vectors length
+##' @importFrom BiocGenerics start
+##' @importFrom BiocGenerics end
+subset.csAnno <- function(x, ... ){
+  
+  
+  index <- paste(seqnames(x@anno),start(x@anno),end(x@anno), sep = "_")
+  # subset the GRanges
+  x@anno <- subset(x@anno, ...)
+  index2 <- paste(seqnames(x@anno),start(x@anno),end(x@anno), sep = "_")
+  
+  # the tssRgion, level, hsaGenomicAnnotation keep unchanged
+  
+  # change the detailGenomicAnnotation
+  x@detailGenomicAnnotation <- x@detailGenomicAnnotation[index %in% index2,]
+  
+  # change the annotation stat 
+  x@annoStat <- ChIPseeker:::getGenomicAnnoStat(x@anno)
+  
+  # change peak number
+  x@peakNum <-  length(x@anno)
+  
+  return(x)
+  
+}
