bf2000ad5a8348f93537105a980d019c5d753573
MingLi-929 <78794151+MingLi-929@users.noreply.github.com>
2021-05-27 15:33:50 +0800

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -75,8 +75,7 @@ getCols <- function(n) {
               "#6a3d9a", "#ffff99", "#b15928")
 
     ## colorRampPalette(brewer.pal(12, "Set3"))(n)
-    ## colorRampPalette(col3)(n)
-    col3[1:n]
+    colorRampPalette(col3)(11)[1:n]
 }
 
 
