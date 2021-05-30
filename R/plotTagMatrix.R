ff789b98ae14aa6c0247be05fc76c734ab5c2663
MingLi-929 <78794151+MingLi-929@users.noreply.github.com>
2021-05-30 23:28:08 +0800

    Update plotTagMatrix.R

--- R/plotTagMatrix.R
+++ R/plotTagMatrix.R
@@ -265,7 +265,7 @@ peakHeatmap.internal <- function(tagMatrix, xlim=NULL, color="red", xlab="", yla
     }
     image(x=xlim, y=1:nrow(tagMatrix),z=t(tagMatrix),useRaster=TRUE, col=cols(length(breaks)-1), yaxt="n", ylab="", xlab=xlab, main=title)
     
-    par(mar=c(5,4,5,5))
+    par(mar=c(2,1,2,2))
     image.scale(tagMatrix, col=cols(length(breaks)-1), breaks=breaks-1e-8, axis.pos=4, add.axis=FALSE)
     axis(4,at=breaks, las=2)                     
 }
