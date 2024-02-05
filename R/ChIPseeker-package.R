833de28ed039979b985b7eb833dd8ba537444b59
Guangchuang Yu <guangchuangyu@gmail.com>
2024-02-06 00:09:45 +0800

    use _PACKAGE to document package manpage

--- R/ChIPseeker-package.R
+++ R/ChIPseeker-package.R
@@ -1,22 +1,5 @@
-#' ChIP-SEQ Annotation, Visualization and Comparison
-#' 
-#'
-#' This package is designed for chip-seq data analysis
-#'
-#' \tabular{ll}{ Package: \tab ChIPseeker\cr Type: \tab Package\cr Version: \tab
-#' 1.5.1\cr Date: \tab 27-04-2015\cr biocViews:\tab ChIPSeq,
-#' Annotation, Software\cr Depends:\tab \cr Imports: \tab methods, ggplot2
-#' \cr Suggests:\tab clusterProfiler, GOSemSim\cr License: \tab
-#' Artistic-2.0\cr }
-#'
-#' @name ChIPseeker-package
-#' @aliases ChIPseeker-package ChIPseeker
-#' @docType package
-#' @author Guangchuang Yu
-#'
-#' Maintainer: Guangchuang Yu \email{guangchuangyu@@gmail.com}
-#' @keywords package
-NULL
+#' @keywords internal
+"_PACKAGE"
 
 
 
